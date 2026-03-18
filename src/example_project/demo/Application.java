2754e3e53448a489fa628ec56660fbd2452d02c2
Prudhvi Krishna <princeprudhvi16@gmail.com>
2026-03-18 10:34:01 +0530

    dev1msg

--- src/example_project/demo/Application.java
+++ src/example_project/demo/Application.java
@@ -1,8 +1,20 @@
 package example_project.demo;
 
 public class Application {
-public static void main(String[] args) {
-	System.out.println("hii guys");
-	System.out.println("dev1 is here");
-}
+
+ 
+    static class Developer {
+        void show() {
+            System.out.println("I am a developer (inner class)");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hii guys");
+        System.out.println("dev1 is here");
+
+  
+        Developer dev = new Developer();
+        dev.show();
+    }
 }
